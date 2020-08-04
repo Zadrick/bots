e132df063277dbf6e74313245f9b23549fc8ebfc
Zadrick <tesla022345@gmail.com>
2020-08-04 18:47:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ svr = express(),
 PORT = process.env.PORT || 80
 
 svr.get(`/initBot`, (req, res)=> {
+    console.log(req.query)
     (req.query.bot && req.query.botKind)? (
         res.send(`bot is active`), [req.query.botKind](req.query.bot)
     ): res.send(`add bots token`)
