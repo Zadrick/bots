0bcbaf34ae711c8838334bb5be7e4bdb5d5cf021
Zadrick <tesla022345@gmail.com>
2020-08-04 06:58:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const r = (f)=> {
 express = require(`express`),
 tessod = require(`./bots/tessod`),
 svr = express(),
-PORT = process.env.PORT || 80;
+PORT = process.env.PORT || 80
 
 svr.get(`/`, (req, res)=> {
-    res.send(`starter`)
+    res.end(`started`)
 })
 
 tessod(`1328623904:AAFmvReJ898Q2Tec0QkhBKIP57f3c3tb_gI`)
