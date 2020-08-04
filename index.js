a1260b700b24d0750b680d4b0120d0a991b30815
Zadrick <tesla022345@gmail.com>
2020-08-04 07:15:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ svr = express(),
 PORT = process.env.PORT || 80
 
 svr.get(`/`, (req, res)=> {
-   res.sendfile(`@/pages/index.php`) 
+   res.sendfile(`~/pages/index.php`) 
 })
 
 tessod(`1328623904:AAFmvReJ898Q2Tec0QkhBKIP57f3c3tb_gI`)
