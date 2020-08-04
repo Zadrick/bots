ba12db60120d5a9ba7818bb5645fa49d4dd58037
Zadrick <tesla022345@gmail.com>
2020-08-04 09:57:59 +0600

    bot

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const r = (f)=> {
     return require(f)
 },
+fs = r(`fs`),
 express = require(`express`),
 tessod = require(`./bots/tessod`),
 svr = express(),
 PORT = process.env.PORT || 80
 
-svr.get(`/`, (req, res)=> {
-   res.sendfile(`src/pages/index.php`) 
+svr.get(`/init-bot`, (req, res)=> {
+    res.send(`hello world`)
+    tessod(req.query.bot)
 })
 
-tessod(URLSearchParams(`bot`))
+svr.get(`/`, (req, res)=> {
+    fs.readFile(`src/pages/index.php`, `utf8`, (err, data)=> {
+        err? console.log(err): res.send(data)
+    })
+})
 
 svr.listen(PORT)
