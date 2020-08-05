0b2fa374469096de8ce127db9fdd435ff915fa8b
Zadrick <tesla022345@gmail.com>
2020-08-05 10:19:35 +0600

    bot

--- index.js
+++ index.js
@@ -13,14 +13,19 @@ welcomeBot = r(`./bots/welcomeBot`),
 badWordsBot = r(`./bots/badWordsBot`)
 
 svr.use(express.static(`public`))
+svr.use(express.urlencoded())
 mongo.connect(`mongodb+srv://tesla022:102410241024Sam>@cluster0.lwr3p.mongodb.net/tessod`, { useUnifiedTopology: true }, async (err, db)=> {
-    console.log(`s`)
+    db.tessod.find()
 })
 
 svr.get(`/`, (req, res)=> {
     fs.readFile(`public/index.html`, `utf8`, (err, data)=> {
-        err? console.log(err): res.send(data)
+        err? console.error(err): res.send(data)
     })
+})
+
+svr.post(`/`, (req, res)=> {
+    console.log(req.body)
 })
 
 svr.listen(PORT)
